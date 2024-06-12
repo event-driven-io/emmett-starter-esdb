dac6e2c87620b4c4a2fecec89c95e035cc513f7b
Oskar Dudycz <oskar.dudycz@gmail.com>
2024-06-12 12:12:57 +0200

    Fixed application setup

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,19 @@ import { getEventStoreDBEventStore } from '@event-driven-io/emmett-esdb';
 import { getApplication, startAPI } from '@event-driven-io/emmett-expressjs';
 import { EventStoreDBClient } from '@eventstore/db-client';
 import type { Application } from 'express';
-import { shoppingCartApi } from './shoppingCarts/api';
+import { guestStayAccountsApi } from './guestStayAccounts/api';
 
 const eventStoreDBClient = EventStoreDBClient.connectionString(
   process.env.ESDB_CONNECTION_STRING ?? `esdb://localhost:2113?tls=false`,
 );
 const eventStore = getEventStoreDBEventStore(eventStoreDBClient);
 
-const getUnitPrice = (_productId: string) => {
-  return Promise.resolve(100);
-};
+const doesGuestStayExist = (_guestId: string, _roomId: string, _day: Date) =>
+  Promise.resolve(true);
 
-const shoppingCarts = shoppingCartApi(
+const shoppingCarts = guestStayAccountsApi(
   eventStore,
-  getUnitPrice,
+  doesGuestStayExist,
   () => new Date(),
 );
 
